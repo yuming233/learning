f6e11af79626f80ac695ce60b732b9d28cf836f9
ylenol <lm464616001@gmail.com>
2019-12-30 15:48:33 +0800

    no message

--- PC端网页特效/仿京东放大镜效果/js/detail.js
+++ PC端网页特效/仿京东放大镜效果/js/detail.js
@@ -15,12 +15,30 @@ window.addEventListener('load', function () {
 
     // 2.鼠标移动的时候，让黄色的盒子跟着鼠标来走
     preview_img.addEventListener('mousemove', function (e) {
-        // 1.先计算出鼠标在盒子内的坐标
+        // (1).先计算出鼠标在盒子内的坐标
         var x = e.pageX - this.offsetLeft;
         var y = e.pageY - this.offsetTop;
 
-        // 盒子的高度300 的一就是150  活写
-        mask.style.left = x - mask.offsetWidth / 2 + 'px';
-        mask.style.top = y - mask.offsetHeight / 2 + 'px';
+        // (2).盒子的高度300 的一就是150  活写
+        // (3).我们mask 移动的距离
+        var maskX = x - mask.offsetWidth / 2;
+        var maskY = y - mask.offsetHeight / 2;
+        // (4) 如果x坐标小于0 就让他停在0的位置
+        var maxX = preview_img.offsetWidth - mask.offsetWidth;
+        var maxY = preview_img.offsetHeight - mask.offsetHeight;
+        // 如果宽在最左边0的坐标就等于0 最右边就用盒子的宽减去子盒子的宽
+        if (maskX <= 0) {
+            maskX = 0;
+        } else if (maskX >= maxX) {
+            maskX = maxX;
+        }
+
+        if (maskY <= 0) {
+            maskY = 0;
+        } else if (maskY >= maxY) {
+            maskY = maxY;
+        }
+        mask.style.left = maskX + 'px';
+        mask.style.top = maskY + 'px';
     })
 })
