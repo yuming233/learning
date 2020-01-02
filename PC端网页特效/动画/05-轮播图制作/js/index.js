7cf2fcd644b111bd10f694215512721b2a62791e
ylenol <lm464616001@gmail.com>
2020-01-02 21:37:39 +0800

    no message

--- PC端网页特效/动画/05-轮播图制作/js/index.js
+++ PC端网页特效/动画/05-轮播图制作/js/index.js
@@ -18,6 +18,16 @@ window.addEventListener('load', function () {
         var li = document.createElement('li');
         // 把小li插入到ol 里面
         ol.appendChild(li);
+
+        // 4.小圆圈的排他思想 我们可以直接在生成小圆圈的同时直接绑定点击事件
+        li.addEventListener('click', function () {
+            // 把所有小li清除 current 类名
+            for (var i = 0; i < ol.children.length; i++) {
+                ol.children[i].className = '';
+            }
+            // 当前的小li 设置current 类名
+            this.className = 'current';
+        })
     }
     // 把ol里面的第一个小li设置类名为current
     ol.children[0].className = 'current';
