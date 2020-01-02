5b5506cc210ab4711006b3afb02fb59c1c112703
ylenol <lm464616001@gmail.com>
2020-01-02 21:27:09 +0800

    no message

--- PC端网页特效/动画/05-轮播图制作/js/index.js
+++ PC端网页特效/动画/05-轮播图制作/js/index.js
@@ -4,9 +4,15 @@ window.addEventListener('load', function () {
     var arrow_r = document.querySelector('.arrow-r');
     var focus = document.querySelector('.focus');
 
-    // 鼠标经过focus 就显示隐藏左右按钮
+    // 2.鼠标经过focus 就显示隐藏左右按钮
     focus.addEventListener('mouseenter', function () {
         arrow_l.style.display = 'block';
         arrow_r.style.display = 'block';
     });
+
+    // 3.动态生成小圆圈  有几张图片 就生成几个小圆圈
+    var ul = focus.querySelector('ul');
+    for (var i = 0; i < ul.children.length; i++) {
+        var li = document.createElement('li');
+    }
 })
