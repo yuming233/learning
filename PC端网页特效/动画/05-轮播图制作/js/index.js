dfd2f308a61e671babddb3e2301411607ddaba38
ylenol <lm464616001@gmail.com>
2020-01-03 15:13:53 +0800

    no message

--- PC端网页特效/动画/05-轮播图制作/js/index.js
+++ PC端网页特效/动画/05-轮播图制作/js/index.js
@@ -33,6 +33,11 @@ window.addEventListener('load', function () {
             // ul 的移动距离就是小圆圈的索引号乘以图片的宽度 注意是负值
             //当我们点击了某个小li 就拿到当前小li 的索引号
             var index = this.getAttribute('index');
+            // 当我们点击了某个小li就要把这个li的索引号给num
+            num = index;
+            // 当我们点击了某个小li就要把这个li的索引号给circle
+            circle = index;
+
             animate(ul, -index * focusWidth);
         })
     }
@@ -45,6 +50,8 @@ window.addEventListener('load', function () {
 
     // 7.点击右侧按钮，图片滚动一张
     var num = 0;
+    // circle 控制小圆圈的播放
+    var circle = 0;
     arrow_r.addEventListener('click', function () {
         if (num == ul.children.length - 1) {
             ul.style.left = 0;
@@ -52,5 +59,19 @@ window.addEventListener('load', function () {
         }
         num++;
         animate(ul, -num * focusWidth);
+
+        // 8.点击右侧按钮，小圆圈跟随一起变化，可以声明一个变量控制小圆圈的播放
+        circle++;
+        // 如果circle == 4 说明走到最后克隆的这张图片了 我们就复原
+        if (circle == ol.children.length) {
+            circle = 0;
+        }
+        // 先清除其余小圆圈的current类名
+        for (var i = 0; i < ol.children.length; i++) {
+            ol.children[i].className = '';
+        }
+        // 留下当前的小圆圈的current类名
+        ol.children[circle].className = 'current';
+
     });
 })
