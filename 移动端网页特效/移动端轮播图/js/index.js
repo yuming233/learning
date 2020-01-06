910f4c3ae1634fa2875bededb061530913e7bb14
ylenol <lm464616001@gmail.com>
2020-01-06 17:35:26 +0800

    no message

--- 移动端网页特效/移动端轮播图/js/index.js
+++ 移动端网页特效/移动端轮播图/js/index.js
@@ -4,6 +4,7 @@ window.addEventListener('load', function () {
     var ul = focus.children[0];
     // 获取focus的宽度
     var w = focus.offsetWidth;
+    var ol = focus.children[1];
     // 2.利用定时器自动轮播图图片
     var index = 0;
     var timer = this.setInterval(function () {
@@ -29,5 +30,10 @@ window.addEventListener('load', function () {
             var translatex = -index * w;
             ul.style.transform = 'translatex(' + translatex + 'px)';
         }
+        // 3.小圆点跟随变化
+        // 把ol里面li带有current类名的选出来去掉类名 remove
+        ol.querySelector('li.current').classList.remove('current');
+        // 让当前索引号的小li 加上 current  add
+        ol.children[index].classList.add('current');
     })
 })
