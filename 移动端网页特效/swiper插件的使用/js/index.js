c8865d4099d0ea728760f926c600f89cbd750393
ylenol <lm464616001@gmail.com>
2020-01-07 14:32:45 +0800

    no message

--- 移动端网页特效/swiper插件的使用/js/index.js
+++ 移动端网页特效/swiper插件的使用/js/index.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function () {
         spaceBetween: 30,
         centeredSlides: true,
         autoplay: {
-            delay: 5000,
+            delay: 2000,
             disableOnInteraction: false,
         },
         pagination: {
