a5b8809a786842caba05794fb7381f90953614d7
ylenol <lm464616001@gmail.com>
2020-01-23 16:11:57 +0800

    no message

--- jQuery学习/todolist/js/todolist.js
+++ jQuery学习/todolist/js/todolist.js
@@ -22,10 +22,12 @@ $(function () {
         var data = getDate();
         // 修改数据
         var index = $(this).attr('id');
+        data.splice(index, 1);  // 从第几个开始删 删几个
         // 保存到本地存储
-        
+        saveDate(data);
         // 重新渲染页面
-    })
+        load();
+    });
 
 
 
