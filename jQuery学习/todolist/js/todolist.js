a8587fb98bf82cf3e1480c2cc47e6e6816bfaeea
ylenol <lm464616001@gmail.com>
2020-01-23 15:44:15 +0800

    no message

--- jQuery学习/todolist/js/todolist.js
+++ jQuery学习/todolist/js/todolist.js
@@ -4,7 +4,11 @@ $(function () {
     $('#title').on('keydown', function (e) {
         if (e.keyCode === 13) {
             // 先读取本地存储原来的数据
-            var local = getData();
+            var local = getDate();
+            // 把local数组进行更新数据 把最新的数据追加给local数组
+            local.push({ title: $(this).val(), done: false });
+            // 把这个数据local存储给本地存储
+            saveDate(local);
 
         }
     });
@@ -14,7 +18,7 @@ $(function () {
 
 
     // 读取本地存储的数据  用的较多 封装一个函数
-    function getData() {
+    function getDate() {
         var data = localStorage.getItem('todolist');
         if (data !== null) {
             // 本地存储里面的数据是字符串格式的 但我们需要的是对象格式
@@ -22,5 +26,10 @@ $(function () {
         } else {
             return [];
         }
+    }
+
+    // 保存本地存储数据
+    function saveDate(data) {
+        localStorage.setItem('todolist', JSON.stringify(data));
     }
 })
