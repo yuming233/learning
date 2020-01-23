f112dcf3d84e71ebe033a86742545248dcd5c4da
ylenol <lm464616001@gmail.com>
2020-01-23 14:41:59 +0800

    no message

--- jQuery学习/todolist/js/todolist.js
+++ jQuery学习/todolist/js/todolist.js
@@ -1,4 +1,26 @@
 $(function () {
+    // 1.按下回车 把完整数据 存储到本地存储里面
+    // 存储的数据格式  var todolist = [{ title: 'xxx', done: false }]
+    $('#title').on('keydown', function (e) {
+        if (e.keyCode === 13) {
+            // 先读取本地存储原来的数据
+            var local = getData();
+
+        }
+    });
 
 
+
+
+
+    // 读取本地存储的数据  用的较多 封装一个函数
+    function getData() {
+        var data = localStorage.getItem('todolist');
+        if (data !== null) {
+            // 本地存储里面的数据是字符串格式的 但我们需要的是对象格式
+            return JSON.parse(data);  // 转换为对象
+        } else {
+            return [];
+        }
+    }
 })
