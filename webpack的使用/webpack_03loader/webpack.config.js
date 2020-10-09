c865bf149606332f876d21b8b612457d9f4262fe
ylenol <lm464616001@gmail.com>
2020-10-10 00:53:51 +0800

    no message

--- webpack的使用/webpack_03loader/webpack.config.js
+++ webpack的使用/webpack_03loader/webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: './src/mian.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: 'dist/'     // 给打包图片加路径
   },
   module: {
     rules: [
@@ -14,7 +15,22 @@ module.exports = {
         // style-loader负责将样式添加到dom中
         // 使用多个loader时，是从右向左运行
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // 图片小于limit时，会将图片编译成base64字符串形式
+              // 当图片大于limit时，需要使用file-loader模块进行加载
+              limit: 8192,
+              name: 'img/[name].[hash:8].[ext]'  //给八位hash值，给打包后图片命名
+            },
+          },
+        ],
       }
+
     ]
   }
 }
