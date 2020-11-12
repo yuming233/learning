46ec7d0bae9172f05f2bc90ef8bb0a2dc7afa61b
yuming233 <464616001@qq.com>
2020-11-12 20:13:52 +0800

    12

--- ChoiceDifficulties/index.js
+++ ChoiceDifficulties/index.js
@@ -61,4 +61,74 @@ filterBtns.addEventListener("click", e => {
       filter: filterOption
     });
   }
+});
+
+
+// 文字出入动画
+const staggeringOption = {
+  delay:300,
+  distance:"50px",
+  duration:500,
+  easing:"ease-in-out",
+  origin:"bottom"
+}
+
+ScrollReveal().reveal(".feature",{...staggeringOption,interval:350})
+ScrollReveal().reveal(".service-item",{...staggeringOption,interval:350})
+
+const dataSectionEl = document.querySelector(".data-section");
+ScrollReveal().reveal(".data-section",{
+  beforeReveal:() => {
+    anime({
+      targets:".data-piece .num",
+      innerHTML:el => {
+        return [0,el.innerHTML];
+      },
+      duration:2000,
+      round:1,
+      easing:"easeInExpo"
+    });
+    dataSectionEl.style.backgroundPosition = "center calc(50% - ${dataSectionEl.getBoundingClientRect().bottom / 5}px)";
+  }
+});
+
+
+window.addEventListener("scroll",() =>{
+  const bottom = dataSectionEl.getBoundingClientRect().bottom;
+  const top = dataSectionEl.getBoundingClientRect().top;
+
+  if(bottom >= 0 && top <= window.innerHeight){
+    dataSectionEl.style.backgroundPosition = "center calc(50% - ${bottom / 5}px)";
+  }
 })
+
+// 定位
+const scroll = new SmoothScroll('nav a[href*="#"],.scrollToTop a[href*="#"]',{
+  header:"header",
+  offset:80
+})
+
+
+// 探索更多按钮
+document.addEventListener("scrollStart",()=>{
+  if(headerEl.classList.contains("open")){
+    headerEl.classList.remove("open")
+  }
+})
+
+
+const exploreBtnEls = document.querySelectorAll(".explore-btn");
+
+exploreBtnEls.forEach(exploreBtnEl=>{
+  exploreBtnEl.addEventListener("click",() =>{
+    scroll.animateScroll(document.querySelector("#about-us"));
+  })
+})
+
+
+// 折叠按钮
+const burgerEl = document.querySelector(".burger");
+burgerEl.addEventListener("click",()=>{
+  headerEl.classList.toggle("open")
+})
+
